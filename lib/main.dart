e3ad77b55b7901d04b0202df76a44eab68121378
Nivas-git07 <itsmenivas007@gmail.com>
2026-02-11 18:41:07 +0000

    understand widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const expense_app());
+}
+
+class expense_app extends StatelessWidget {
+  const expense_app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const expense_home());
+  }
+}
+
+class expense_home extends StatelessWidget {
+  const expense_home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Expense tracker")),
+      body: Text("body content"),
+      
+    );
+  }
 }
