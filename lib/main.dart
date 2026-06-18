dcc4abb1f03d18ce008b9660595c05edd23ac361
Nivas-git07 <itsmenivas007@gmail.com>
2026-06-18 14:34:52 +0000

    add loginn screen for ecommerce site

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const expense_app());
+  runApp(const MyApp());
 }
 
-class expense_app extends StatelessWidget {
-  const expense_app({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const expense_home());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Loginscreen(),
+    );
   }
 }
 
@@ -21,7 +24,167 @@ class expense_home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("Expense tracker")),
       body: Text("body content"),
-      
+    );
+  }
+}
+
+class Loginscreen extends StatelessWidget {
+  const Loginscreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              Container(
+                width: 50,
+                height: 50,
+
+                decoration: const BoxDecoration(
+                  color: Color(0xff0D1B3D),
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(Icons.attach_money, color: Colors.white),
+              ),
+
+              const SizedBox(height: 40),
+              const Text(
+                "welcome Back !",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+
+              Text(
+                "please enter your details",
+                style: TextStyle(color: Colors.grey.shade600, fontSize: 16),
+              ),
+
+              const SizedBox(height: 30),
+
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "example@gmail.com",
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "* * * * * * *",
+                  prefixIcon: const Icon(Icons.lock_outline),
+                  suffixIcon: const Icon(Icons.visibility_off),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                width: double.infinity,
+                height: 55,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xff081B4B),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+
+              const Center(
+                child: Text(
+                  "or sign with",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+              const SizedBox(height: 25),
+
+              Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.g_mobiledata,
+                        color: Colors.red,
+                        size: 28,
+                      ),
+                      label: const Text(
+                        "Google",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        minimumSize: const Size(0, 55),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadiusGeometry.circular(30),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: OutlinedButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.apple, color: Colors.black),
+                      label: const Text(
+                        "Apple",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        minimumSize: const Size(0, 55),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              const Spacer(),
+
+              Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text("Don't have an acoount ?"),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Register",
+                        style: TextStyle(color: Colors.orange),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
